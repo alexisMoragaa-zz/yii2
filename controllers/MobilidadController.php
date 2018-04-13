7e52123d23a7bb71a33a41a01fadf9d4cdf0cbb0
Alexis Moraga <alexis.moraga.gallardo@gmail.com>
2018-04-13 16:05:11 -0300

    comenzamos con la creacion de la accion encargada de subir el archivo excel a la bd

--- controllers/MobilidadController.php
+++ controllers/MobilidadController.php
@@ -28,13 +28,38 @@ class MobilidadController extends \yii\web\Controller
 
         if($model->upload()){
           //validamos si la funcion upload del modelo LoadExcel nos retorna true. si nos retorna true el el archivo se cargo con exito
-            Yii::$app->session->setFlash('success','Tu archivo excel se cargo con exito');
+            Yii::$app->session->setFlash('info','Tu archivo excel se cargo con exito');
           //si la carga del archivo fue exitosa retornamos un mensaje de session indicando que el archivo fue cargado con extito
+
+          $ruta ='uploads/'.$model->excelFile->name;
+          //guardamos la ruta del archivo que cargamos en la variable ruta para enviarla al metodo ImportExcel que cargara el aechivo en la base de datos
+         $this->ImportExcel($ruta);
+         //si el archivo se cargo con exito redireccionamos a un metodo que lo insertara en la base de datos
         }
       }
 
       return $this->render('loadExcel',['model'=>$model]);
       //si no viene una peticion de tipo post cuando llamamos a la accion retornamos la vista load excel con una instancia del modelo
+    }
+
+    public function ImportExcel($ruta){ //creamos uns funcion que tomara el archivo excel cargado y lo insertara en la base de datos usando la libreria phpexcel
+        Yii::$app->session->setFlash('success',' fueste redirigido a otro metodo y me entragaste este ruta '.$ruta);
+        //añadimos un mensaje flash indicando que fuimos redireccionados con exito
+
+        try{
+          $inputFiletype = \PHPExcel_IOFactory::indentify($ruta);
+          //establecemos el tipo de archivo identificando el mismo mediate la ruta
+          $objReader = \PHPExcel_IOFactory::CreateReader($inputFiletype);
+          //creamos un objeto para leerlo usando el $inputFiletype que creamos con la ruta
+          $objPHPExcel = $objReader->load($ruta);
+          //creamos un objeto phpexcel cargando el $objReader creado anteriormente, el cual cargamos desde la ruta
+        }catch(Exception $ex){
+          //si el try falla matamos el proceso y retornamos los errores
+            die(ex);
+        }
+
+
+
     }
 
 
